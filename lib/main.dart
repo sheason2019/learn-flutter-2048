f36afa73d494ed40bf462d901e83965f07f8eefa
sheason <811627877@qq.com>
2021-11-21 22:27:41 +0800

    feat: flutter layout

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:english_words/english_words.dart';
 
 void main() {
   runApp(const MyApp());
@@ -8,100 +7,91 @@ void main() {
 class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo Theme',
-      theme: ThemeData(primarySwatch: Colors.blue),
-      home: const Scaffold(
-          body: Center(
-        child: RandomWords(),
-      )),
-    );
-  }
-}
-
-class RandomWords extends StatefulWidget {
-  const RandomWords({Key? key}) : super(key: key);
-
-  @override
-  createState() => RandomWordsState();
-}
-
-class RandomWordsState extends State<RandomWords> {
-  final _suggestions = <WordPair>[];
-  final _biggerFont = const TextStyle(fontSize: 18.0);
-  final _saved = <WordPair>{};
-
-  void _pushSaved() {
-    Navigator.of(context).push(MaterialPageRoute(builder: (context) {
-      final tiles = _saved.map((pair) {
-        return ListTile(
-            title: Text(
-          pair.asPascalCase,
-          style: _biggerFont,
+    Widget titleSection = Container(
+        padding: const EdgeInsets.all(32.0),
+        child: Row(
+          children: [
+            Expanded(
+                child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Container(
+                    padding: const EdgeInsets.only(bottom: 8.0),
+                    child: const Text('Oeschinen Lake Campground',
+                        style: TextStyle(fontWeight: FontWeight.bold))),
+                Text(
+                  'Kandersteg, Switzerland',
+                  style: TextStyle(color: Colors.grey[500]),
+                ),
+              ],
+            )),
+            Icon(Icons.star, color: Colors.red[500]),
+            const Text('41'),
+          ],
         ));
-      });
-      final divided =
-          ListTile.divideTiles(tiles: tiles, context: context).toList();
-      return Scaffold(
-          appBar: AppBar(title: const Text('Saved Suggestions')),
-          body: ListView(
-            children: divided,
-          ));
-    }));
-  }
 
-  Widget _buildSuggestions() {
-    return ListView.builder(
-      padding: const EdgeInsets.all(16.0),
-      itemBuilder: (context, i) {
-        if (i.isOdd) return const Divider();
-        final index = i ~/ 2;
-        if (index >= _suggestions.length) {
-          _suggestions.addAll(generateWordPairs().take(10));
-        }
-        return _buildRow(_suggestions[index]);
-      },
+    Column buildButtonColumn(IconData icon, String label) {
+      Color color = Theme.of(context).primaryColor;
+
+      return Column(
+        mainAxisSize: MainAxisSize.min,
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Icon(icon, color: color),
+          Container(
+              margin: const EdgeInsets.only(top: 8.0),
+              child: Text(label,
+                  style: TextStyle(
+                    fontSize: 12.0,
+                    fontWeight: FontWeight.w400,
+                    color: color,
+                  )))
+        ],
+      );
+    }
+
+    Widget buttonSection = Row(
+      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+      children: [
+        buildButtonColumn(Icons.call, 'CALL'),
+        buildButtonColumn(Icons.near_me, 'ROUTE'),
+        buildButtonColumn(Icons.share, 'SHARE')
+      ],
     );
-  }
 
-  Widget _buildRow(WordPair pair) {
-    final alreadySaevd = _saved.contains(pair);
-    return ListTile(
-      title: Text(
-        pair.asPascalCase,
-        style: _biggerFont,
+    Widget textSection = Container(
+      padding: const EdgeInsets.all(32.0),
+      child: const Text(
+        '''
+        Here should describe Lake Oeshinen.
+        ''',
+        softWrap: true,
       ),
-      trailing: Icon(
-        alreadySaevd ? Icons.favorite : Icons.favorite_border,
-        color: alreadySaevd ? Colors.red : null,
-      ),
-      onTap: () {
-        setState(() {
-          if (alreadySaevd) {
-            _saved.remove(pair);
-          } else {
-            _saved.add(pair);
-          }
-        });
-      },
     );
-  }
 
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(primarySwatch: Colors.blue),
+      home: Scaffold(
         appBar: AppBar(
-          title: const Text("Startup Name Generate"),
-          actions: <Widget>[
-            IconButton(
-              icon: const Icon(Icons.list),
-              onPressed: _pushSaved,
-            )
+          title: const Text('Flutter Layout'),
+        ),
+        body: ListView(
+          children: [
+            Image.asset(
+              'image/lake.jpeg',
+              width: 600,
+              height: 240,
+              fit: BoxFit.cover,
+            ),
+            titleSection,
+            buttonSection,
+            textSection,
           ],
         ),
-        body: _buildSuggestions());
+      ),
+    );
   }
 }
